9eb6e5a52c4809ee3af14c30b944809039ae5912
Ameya Joshi <mcgenius2@gmail.com>
2020-02-23 14:12:32 -0600

    Finished basic positioning for home, needs styling

--- src/Home.js
+++ src/Home.js
@@ -5,16 +5,31 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <Container>
-    <Row>
-      <Col><h1>Preserving Nature Through Education</h1><br/><h3>Learn more about
-        endangered species and the national parks protecting them</h3></Col>
-      <Col md={{span: 4, offset: 2}}><Image src={ logo } fluid /></Col>
-    </Row>
-    </Container>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+        position: 'absolute', top: '15%', bottom: '10%', left: '0%', right: '0%'
+      }}>
+      <Container>
+        <Row>
+          <Col>
+            <br/><h1>Preserving Nature Through Education</h1>
+            <h4>Learn more about endangered species and the national parks protecting
+            them</h4><br/><br/><br/><br/>
+            <Button variant="danger"><Link to="/Parks">  Get Started  </Link></Button>
+          </Col>
+          <Col xs={6}><Image src={ logo } fluid /></Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
 
