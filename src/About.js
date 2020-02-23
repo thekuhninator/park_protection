06cf4a8a6c7ab8273d0d8be548e4df9c0e800f44
Dylan Kan <dylan.kan67@utexas.edu>
2020-02-23 17:02:23 -0600

    about us

--- src/About.js
+++ src/About.js
@@ -2,7 +2,21 @@ import React, {Component} from 'react';
 
 function About() {
   return ( 
-    <div>Hello</div>
+    <div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h1 align="center">About Us</h1>
+      <br></br>
+      <h5 align="center">Project Goal:</h5>
+      <p align="center"> Many species are dying out in recent years, whether 
+        it be due to global warming or other environmental factors. This website
+        helps those who want to witness or take action in preservation efforts
+        locate the appropriate help centers. Our websie grabs data on endangered 
+        animal and plant species and groups them with national park centers based
+        on location in hopes to spread awareness of what states may have efforts
+        already in place for preservation. </p>
+    </div>
 
   );
 }
