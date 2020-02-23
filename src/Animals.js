8d8a7e279dbbf933d5a6b742a36ac023926f6a6b
Poisonthorns <evanssm@utexas.edu>
2020-02-22 18:00:47 -0600

    Adding empty files for a few pages

--- src/Animals.js
+++ src/Animals.js
@@ -1,35 +1,8 @@
 import React, {Component} from 'react';
-import logo from './logo.png';
-import './App.css';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Form, FormControl, Button } from 'react-bootstrap';
 
-function Home() {
+function Animals() {
   return (
-    <div className="Home">
-    <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Navbar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
-export default Home;
+export default Animals;
