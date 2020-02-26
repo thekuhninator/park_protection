8ea7bc371c6b63dc2a9bab7f76fb3a307fe79398
Roman Kuhn <kuhn@cs.utexas.edu>
2020-02-26 14:56:26 -0600

    made significant progress on the about page

--- src/About.js
+++ src/About.js
@@ -4,6 +4,8 @@ import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import CardDeck from 'react-bootstrap/CardDeck';
 import Image from 'react-bootstrap/Image'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 import roman from './Assets/img/headshots/roman.jpeg'
 import dylan from './Assets/img/headshots/dylan.jpg'
@@ -12,6 +14,13 @@ import skylore from './Assets/img/headshots/skylore.jpeg'
 import jordan from './Assets/img/headshots/jordan.jpg'
 import pedro from './Assets/img/headshots/pedro.jpeg'
 
+const AboutTitle = styled('h1')`
+color: black; font-family: 'Raleway',sans-serif; font-size: 62px; font-weight: 800; line-height: 72px; margin: 0 0 24px; text-align: center; text-transform: uppercase;
+`
+
+const Paragraph = styled('p')`
+	color: black; font-family: 'Raleway',sans-serif; font-size: 18px; font-weight: 500; line-height: 32px; margin: 0 0 24px;
+`
 
 const AboutBox = styled('div')`
 	text-align: left;
@@ -38,15 +47,17 @@ function About() {
   return (
       <Container>
 			<Container>
-	        <h1>About Us</h1>
+			<br></br>
+
+	        <AboutTitle> About Us</AboutTitle>
 	        <br></br>
-	        <p> Many species are dying out in recent years, whether
+	        <Paragraph > Many species are dying out in recent years, whether
 	          it be due to global warming or other environmental factors. This website
 	          helps those who want to witness or take action in preservation efforts
 	          locate the appropriate help centers. Our websie grabs data on endangered
 	          animal and plant species and groups them with national park centers based
 	          on location in hopes to spread awareness of what states may have efforts
-	          already in place for preservation. </p>
+	          already in place for preservation. </ Paragraph>
 			</Container>
 			<CardDeck>
 				<Card>
@@ -130,59 +141,63 @@ function About() {
 						</Card.Footer>
 					</Card>
 			</CardDeck>
+
+			<Container>
 			<h1> Repository Stats </h1>
-			36 commits 1000 issues 9999 unit tests
-			<h1> Data Sources </h1>
-			Data was collected from my ass.
-			<h1> Tools </h1>
-			Tools used for scraping stuff included...
-			Gitlab Repository
-			Postman Api
 
-			<CardDeck>
-				<Card>
-					<Card.Img variant="top" src={skylore} />
-					<Card.Body>
-						<Card.Title> Skylore Evans </Card.Title>
-						<Card.Text>
-							Ameya is a super cool and nice guy and he is my friend.
-						</Card.Text>
-					</Card.Body>
-					<Card.Footer>
-						commits and whatnot
-					</Card.Footer>
-				</Card>
-				<Card>
-					<Card.Img variant="top" src={jordan} />
-					<Card.Body>
-						<Card.Title> Jordan Bogaards </Card.Title>
-						<Card.Text>
-							Ameya is a super cool and nice guy and he is my friend.
-						</Card.Text>
-					</Card.Body>
-					<Card.Footer>
-						commits and whatnot
-					</Card.Footer>
+				<Row>
+					<Col> <h2> XXX Commits </h2> </Col>
+					<Col> <h2> XXX Issues </h2> </Col>
+					<Col> <h2> XXX Unit Tests </h2> </Col>
+				</Row>
+			</Container>
+
+			<Container>
+				<h1> Data Sources </h1>
+				<p> Data sources were collected from _____. </p>
+			</Container>
+
+			<Container>
+				<h1> Tools </h1>
+				<CardDeck>
+					<Card>
+						<Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/AWS_Simple_Icons_AWS_Cloud.svg/1280px-AWS_Simple_Icons_AWS_Cloud.svg.png" />
+						<Card.Body>
+							<Card.Title> AWS </Card.Title>
+							<Card.Text>
+								Amazon Web Services (AWS) is a platform that allows us to host our website.
+							</Card.Text>
+						</Card.Body>
+					</Card>
+					<Card>
+						<Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" />
+						<Card.Body>
+							<Card.Title> React Bootstrap</Card.Title>
+							<Card.Text>
+								React Bootstrap is a front-end tool that allows us to create nice web pages.
+							</Card.Text>
+						</Card.Body>
+					</Card>
+
+					<Card>
+						<Card.Img variant="top" src="https://img.stackshare.io/service/1336/xWMRvm_5_400x400.png" />
+						<Card.Body>
+							<Card.Title> Postman </Card.Title>
+							<Card.Text>
+								Postman is a tool that generates useful API for our functions.
+							</Card.Text>
+						</Card.Body>
+					</Card>
+			</CardDeck>
+			</Container>
+
+			<br />
+
+			<h2><a href="https://gitlab.com/thekuhninator/parks_protection"> Gitlab Repository </a></h2>
+			<h2> Postman Api </h2>
 
 
-
-				</Card>
-				<Card>
-					<Card.Img variant="top" src={pedro} />
-					<Card.Body>
-						<Card.Title> Pedro Silva </Card.Title>
-						<Card.Text>
-							Ameya is a super cool and nice guy and he is my friend.
-						</Card.Text>
-					</Card.Body>
-					<Card.Footer>
-						Commits:
-						Total Commits:
-					</Card.Footer>
-				</Card>
-		</CardDeck>
 		</Container>
-
   );
 }
 
