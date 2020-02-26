55eb174b7ba9268ce0771f9943c54253459c0cff
Roman Kuhn <kuhn@cs.utexas.edu>
2020-02-26 14:03:22 -0600

    Added cards to the about page as well as some information below

--- src/About.js
+++ src/About.js
@@ -1,5 +1,16 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Image from 'react-bootstrap/Image'
+
+import roman from './Assets/img/headshots/roman.jpeg'
+import dylan from './Assets/img/headshots/dylan.jpg'
+import ameya from './Assets/img/headshots/ameya.jpeg'
+import skylore from './Assets/img/headshots/skylore.jpeg'
+import jordan from './Assets/img/headshots/jordan.jpg'
+import pedro from './Assets/img/headshots/pedro.jpeg'
 
 
 const AboutBox = styled('div')`
@@ -13,21 +24,120 @@ const AboutBox = styled('div')`
   border-radius: 40px 20px;
 `;
 
+const ImageBoxLong = styled('img')`
+	width: 500px;
+	height: 500px;
+`;
+
+
 function About() {
   return (
-    <AboutBox>
+		<Container>
       <h1 align="center">About Us</h1>
       <br></br>
-      <h5 align="center">Project Goal:</h5>
+      <h4 align="center">Project Goal:</h4>
       <p align="center"> Many species are dying out in recent years, whether
         it be due to global warming or other environmental factors. This website
-        helps those who want to witness or take action in preservation efforts
+        helps those who want to witness4 or take action in preservation efforts
         locate the appropriate help centers. Our websie grabs data on endangered
         animal and plant species and groups them with national park centers based
         on location in hopes to spread awareness of what states may have efforts
         already in place for preservation. </p>
-    </AboutBox>
 
+			<div>
+			<CardDeck>
+				<Card>
+					<Card.Img variant="top" src={roman} />
+					<Card.Body>
+						<Card.Title> Roman Kuhn </Card.Title>
+						<Card.Text>
+							Ameya is a super cool and nice guy and he is my friend.
+						</Card.Text>
+					</Card.Body>
+					<Card.Footer>
+						commits and whatnot
+					</Card.Footer>
+				</Card>
+				<Card>
+					<Card.Img variant="top" src={dylan} />
+					<Card.Body>
+						<Card.Title> Dylan Kan </Card.Title>
+						<Card.Text>
+							Ameya is a super cool and nice guy and he is my friend.
+						</Card.Text>
+					</Card.Body>
+					<Card.Footer>
+						commits and whatnot
+					</Card.Footer>
+				</Card>
+				<Card>
+					<Card.Img variant="top" src={ameya} />
+					<Card.Body>
+						<Card.Title> Ameya Joshi </Card.Title>
+						<Card.Text>
+							Ameya is a super cool and nice guy and he is my friend.
+						</Card.Text>
+					</Card.Body>
+					<Card.Footer>
+						commits and whatnot
+					</Card.Footer>
+				</Card>
+				</CardDeck>
+				</div>
+				<br />
+
+				<CardDeck>
+					<Card>
+						<Card.Img variant="top" src={skylore} />
+						<Card.Body>
+							<Card.Title> Skylore Evans </Card.Title>
+							<Card.Text>
+								Ameya is a super cool and nice guy and he is my friend.
+							</Card.Text>
+						</Card.Body>
+						<Card.Footer>
+							commits and whatnot
+						</Card.Footer>
+					</Card>
+					<Card>
+						<Card.Img variant="top" src={jordan} />
+						<Card.Body>
+							<Card.Title> Jordan Bogaards </Card.Title>
+							<Card.Text>
+								Ameya is a super cool and nice guy and he is my friend.
+							</Card.Text>
+						</Card.Body>
+						<Card.Footer>
+							commits and whatnot
+						</Card.Footer>
+
+
+
+					</Card>
+					<Card>
+						<Card.Img variant="top" src={pedro} />
+						<Card.Body>
+							<Card.Title> Pedro Silva </Card.Title>
+							<Card.Text>
+								Ameya is a super cool and nice guy and he is my friend.
+							</Card.Text>
+						</Card.Body>
+						<Card.Footer>
+							Commits:
+							Total Commits:
+						</Card.Footer>
+					</Card>
+			</CardDeck>
+			<h1> Repository Stats </h1>
+			36 commits 1000 issues 9999 unit tests
+			<h1> Data Sources </h1>
+			Data was collected from my ass.
+			<h1> Tools </h1>
+			Tools used for scraping stuff included...
+			Gitlab Repository
+			Postman Api
+
+		</Container>
   );
 }
 
