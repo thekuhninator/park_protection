9ae68521a1d2e99b1d5a21551b6d580d0a5c4d4a
Ameya Joshi <mcgenius2@gmail.com>
2020-03-05 11:48:51 -0600

    Removed foreign animals, added listing date.

--- scraping/animals.py
+++ scraping/animals.py
@@ -3,15 +3,15 @@ import requests
 
 endpoint = "https://ecos.fws.gov/ecp/pullreports/catalog/species/report/species/export"
  
-animalColumns = "columns=/species@sid,cn,sn,status,gn,dps,is_aquatic,is_bcc,country;/species/range_state@abbrev;/species/conservation_plans@plan_title"
+animalColumns = "columns=/species@sid,cn,sn,status,listing_date,gn,dps,is_aquatic,is_bcc;/species/range_state@abbrev;/species/conservation_plans@plan_title"
 
 # prevent null = scientific name, common name, domestic/foreign
 # allowed null = dps, aquatic, bcc, range_state, conservation_plan
-validFilters = "filter=/species@sn != ''&filter=/species@sn is not null&filter=/species@cn != ''&filter=/species@cn is not null&filter=/species@cn != 'None'&filter=/species@country != ''&filter=/species@country is not null"
+validFilters = "filter=/species@sn != ''&filter=/species@sn is not null&filter=/species@cn != ''&filter=/species@cn is not null&filter=/species@cn != 'None'"
 
 statusFilters = "filter=/species@status = 'Endangered' or /species@status = 'Recovery' or /species@status = 'Threatened'"
 
-# 1365
+# 653
 animalFilters = "filter=/species@gn = 'Amphibians' or /species@gn = 'Arachnids' or /species@gn = 'Birds' or /species@gn = 'Clams' or /species@gn = 'Corals' or /species@gn = 'Crustaceans' or /species@gn = 'Fishes' or /species@gn = 'Insects' or /species@gn = 'Mammals' or /species@gn = 'Reptiles' or /species@gn = 'Snails'"
 
 r = requests.get(endpoint + "?" + animalColumns + "&" + validFilters + "&" + statusFilters + "&" + animalFilters)
@@ -43,11 +43,11 @@ for animalData in rawData:
 		animalDict['commonName'] = animalData[1]
 		animalDict['scientificName'] = animalData[2]['value']
 		animalDict['status'] = animalData[3]
-		animalDict['group'] = animalData[4]
-		animalDict['dps'] = animalData[5]
-		animalDict['aquatic'] = animalData[6]
-		animalDict['bcc'] = animalData[7]
-		animalDict['country'] = animalData[8]
+		animalDict['date'] = animalData[4]
+		animalDict['group'] = animalData[5]
+		animalDict['dps'] = animalData[6]
+		animalDict['aquatic'] = animalData[7]
+		animalDict['bcc'] = animalData[8]
 		animalDict['states'] = list() if animalData[9] is None else [animalData[9]]
 		animalDict['plan'] = None if animalData[10] is None else animalData[10]['value']
 		animalsData.append(animalDict)
@@ -55,8 +55,10 @@ for animalData in rawData:
 # turn states list into states string
 for i in range(len(animalsData) - 1, -1, -1):
 	animalsData[i]['states'] = ", ".join(animalsData[i]['states'])
-	# if animalsData[i]['states'] == "":
-	# 	del animalsData[i]
+	if animalsData[i]['plan'] is None:
+		animalsData[i]['plan'] = "None"
+	if animalsData[i]['states'] == "":
+		del animalsData[i]
 
 print(len(animalsData))
 print(animalsData)
