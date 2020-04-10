93564f05cac67cd19d2ee2b0fc49d4aa4c2249fa
Dylan Kan <dylan.kan67@utexas.edu>
2020-04-10 11:20:39 -0500

    remove quotes and brackets from wiki parse

--- scraping/wikidata.py
+++ scraping/wikidata.py
@@ -49,9 +49,15 @@ pagetext = DATA["parse"]["wikitext"]["*"]
 # remove speciesbox
 paragraph = re.sub('({{([^}])+}})', '', pagetext)
 paragraph = re.search('(\'\'\'(.+\n+)+?(==))', paragraph).group()
+# remove next paragraph header
 paragraph = re.sub('(==)', '', paragraph)
 # parse and remove tags
-print(re.sub('(<.+?>)', '', paragraph))
+paragraph = re.sub('(<.+?>)', '', paragraph)
+# remove quotes and quotes and brackets
+paragraph = re.sub('(\[)', '', paragraph)
+paragraph = re.sub('(\])', '', paragraph)
+paragraph = re.sub('\'', '', paragraph)
+print(paragraph)
 
 # Animal pages tend to have a "The " that will get parsed out by the regex. We can add this in manually if we know we're searching an animal.
 
